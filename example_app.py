7acdf175b3a83228002a46ab3dc143734b43876b
Jason Bachman <jason.bachman@gmail.com>
2011-09-27 14:36:56 -0400

    some helpful comments

--- example_app.py
+++ example_app.py
@@ -2,6 +2,7 @@
 
 from bb_score_objects import *
 
+# set up teams
 phillies = Team(name="Phillies", hometown="Philadelphia", manager="Charlie Manuel", league="NL")
 braves = Team(name="Braves", hometown="Atlanta", manager="Bobby Cox", league="NL")
 
@@ -29,12 +30,15 @@ gonzalez = Player(braves, "Alex Gonzalez", 2, 6, "R", "R")
 delgado = Player(braves, "Randall Delgado", 40, 1, "R", "R")
 lineup_home_110926 = Lineup(team=braves, one=bourn, two=prado, three=jones, four=uggla, five=freeman, six=mccann, seven=diaz, eight=gonzalez, nine=delgado)
 
+# set up game
 phillies_braves_110926 = Game("110926", braves, lineup_home_110926, phillies, lineup_away_110926)
 
+# print out game and lineup info
 phillies_braves_110926.game_info()
 lineup_away_110926.lineup_info()
 lineup_home_110926.lineup_info()
 
+# save game data to xml
 phillies_braves_110926.save_to_xml()
 
 
